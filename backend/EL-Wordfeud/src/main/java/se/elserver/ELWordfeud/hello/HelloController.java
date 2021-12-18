fca898c61321c13fce5c0c61b031abe1076ad443
erra-ch <73989943+erra-ch@users.noreply.github.com>
2021-12-18 19:52:50 +0100

    Added decorator for CORS

--- backend/EL-Wordfeud/src/main/java/se/elserver/ELWordfeud/hello/HelloController.java
+++ backend/EL-Wordfeud/src/main/java/se/elserver/ELWordfeud/hello/HelloController.java
@@ -1,12 +1,14 @@
 package se.elserver.ELWordfeud.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class HelloController {
 
     @Autowired // This is dependency injection. HelloService is a singleton (service)
