ed5c1d5234f645564e878df429584bd039a1d0b9
erra-ch <73989943+erra-ch@users.noreply.github.com>
2021-12-18 09:05:50 +0100

    Initialized spring project

---
+++ backend/EL-Wordfeud/src/main/java/se/elserver/ELWordfeud/ElWordfeudApplication.java
@@ -0,0 +1,13 @@
+package se.elserver.ELWordfeud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElWordfeudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElWordfeudApplication.class, args);
+	}
+
+}
