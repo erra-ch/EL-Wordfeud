478452b10d71c58e944783fb4381f485f6d3f209
Jonas Nordin <jonnordi@student.chalmers.se>
2022-01-05 17:24:27 +0100

    Dynamic urls test use

--- backend/EL-Wordfeud/src/main/java/se/elserver/ELWordfeud/hello/HelloController.java
+++ backend/EL-Wordfeud/src/main/java/se/elserver/ELWordfeud/hello/HelloController.java
@@ -3,6 +3,7 @@ package se.elserver.ELWordfeud.hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class HelloController {
     @GetMapping("/hello")
     public List<Hello> hello() {
         return helloService.getHelloList();
+    }
+
+    @GetMapping("/hello/{id}")
+    public String helloId(@PathVariable int id) {
+        return "You have requested hallo with id: " + id;
     }
 }
